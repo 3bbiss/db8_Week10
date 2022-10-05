d88ae39088d54b9af35a2584cc6d00700bfed107
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-10-05 11:21:56 -0400

    add PERSON with validation working

---
+++ BookClubLab/Controllers/PersonController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookClubLab.Models;
+
+namespace BookClubLab.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Person> persons = DAL.GetAllPeople();
+            return View(persons);
+        }
+
+        public IActionResult AddForm()
+        {
+            return View();
+        }
+
+        public IActionResult Add(Person per)
+        {
+
+            bool isValid = true;
+
+            if (per.firstname == null)
+            {
+                ViewBag.fnameMessage = "First name required";
+                isValid = false;
+            }
+            if (per.lastname == null)
+            {
+                ViewBag.lnameMessage = "Last name required";
+                isValid = false;
+            }
+            if (per.email == null)
+            {
+                ViewBag.emailMessage = "Email required";
+                isValid = false;
+            }
+            if (isValid)
+            {
+                DAL.InsertPerson(per);
+                return Redirect("/person");
+            }
+            else
+            {
+                List<Person> persons = DAL.GetAllPeople();
+                ViewBag.fName = per.firstname;
+                ViewBag.lName = per.lastname;
+                ViewBag.email = per.email;
+                return View("AddForm", persons);
+            }
+        }
+
+    }
+}
---
+++ BookClubLab/Models/DAL.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using System.Net.Http.Headers;
+
+namespace BookClubLab.Models
+{
+    public class DAL
+    {
+        public static MySqlConnection DB = new MySqlConnection("Server=127.0.0.1;Database=bookclub;Uid=root;Pwd=abc123;");
+
+
+        public static List<Person> GetAllPeople()
+        {
+            return DB.GetAll<Person>().ToList();
+        }
+
+        public static Person GetOnePeople(int id)
+        {
+            return DB.Get<Person>(id);
+        }
+
+        public static Person InsertPerson(Person per)
+        {
+            DB.Insert<Person>(per);
+            return per;
+        }
+
+
+        public static void DeletePerson(int id)
+        {
+            DB.Execute("delete from presentation where personid=@personid", new {personid = id});
+
+            Person per = new Person() { id = id};
+            DB.Delete<Person>(per);
+        }
+
+        public static void UpdatePerson(Person per)
+        {
+            DB.Update<Person>(per);
+        }
+
+
+
+        public static List<Presentation> GetAllPresentations()
+        {
+            return DB.GetAll<Presentation>().ToList();
+        }
+
+        public static Presentation GetOnePresentation(int id)
+        {
+            return DB.Get<Presentation>(id);
+        }
+
+        public static Presentation InsertPresentation(Presentation pres)
+        {
+            DB.Insert(pres);
+            return pres;
+        }
+
+        public static void DeletePresentation(int id)
+        {
+            Presentation pres = new Presentation() { id = id };
+            DB.Delete(pres);
+        }
+
+        public static void UpdatePresentation(Presentation pres)
+        {
+            DB.Update(pres);
+        }
+    }
+}
