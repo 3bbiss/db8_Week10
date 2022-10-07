960005ff8afbbee559b9c32a8868c4f7f947a9cb
Ali Abbiss <aliabbiss@rocketmortgage.com>
2022-10-07 00:58:55 -0400

    edit person working with validation

--- BookClub2/Controllers/PersonController.cs
+++ BookClub2/Controllers/PersonController.cs
@@ -42,6 +42,8 @@ namespace BookClub2.Controllers
             }
             else
             {
+                // why were we passing both the 
+                // AddForm view AND the people list?
                 //List<Person> people = DAL.GetAllPeople();
                 ViewBag.firstname = per.firstname;
                 ViewBag.lastname = per.lastname;
@@ -49,6 +51,49 @@ namespace BookClub2.Controllers
                 return View("AddForm");
             }
         }
+
+        public IActionResult EditForm(int id)
+        {
+            return View(DAL.GetOnePerson(id));
+        }
+
+        public IActionResult SaveChanges(Person per)
+        {
+            bool isValid = true;
+
+            if (per.firstname == null)
+            {
+                isValid = false;
+                ViewBag.firstnameMessage = "Enter first name";
+            }
+            if (per.lastname == null)
+            {
+                isValid = false;
+                ViewBag.lastnameMessage = "Enter last name";
+            }
+            if (per.email == null)
+            {
+                isValid = false;
+                ViewBag.emailMessage = "Enter email";
+            }
+            if (isValid)
+            {
+                DAL.UpdatePerson(per);
+                return Redirect("/person");
+            }
+            else
+            {
+                ViewBag.id = per.id;
+                ViewBag.firstname = per.firstname;
+                ViewBag.lastname = per.lastname;
+                ViewBag.email = per.email;
+                //return View("EditForm");
+                // WHY DIDN'T THIS WORK???
+                return View("EditForm", new Person { id = per.id });
+            }
+        }
+
+
 
     }
 }
